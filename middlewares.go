e4db9581a6d82e52223dd504a291656b6ad05cca
dev <xxuechin@gmail.com>
2023-04-26 01:18:25 +0800

    page

--- middlewares.go
+++ middlewares.go
@@ -1,6 +1,5 @@
 package novelsvc
 
-
 import (
 	"context"
 	"time"
@@ -25,65 +24,9 @@ type loggingMiddleware struct {
 	logger log.Logger
 }
 
-func (mw loggingMiddleware) PostProfile(ctx context.Context, p Profile) (err error) {
+func (mw loggingMiddleware) GetListByKeyword(ctx context.Context, keyword string) (err error) {
 	defer func(begin time.Time) {
-		mw.logger.Log("method", "PostProfile", "id", p.ID, "took", time.Since(begin), "err", err)
+		mw.logger.Log("method", "GetListByKeyword", "keyword=", keyword, "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return mw.next.PostProfile(ctx, p)
-}
-
-func (mw loggingMiddleware) GetProfile(ctx context.Context, id string) (p Profile, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetProfile", "id", id, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.GetProfile(ctx, id)
-}
-
-func (mw loggingMiddleware) PutProfile(ctx context.Context, id string, p Profile) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "PutProfile", "id", id, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.PutProfile(ctx, id, p)
-}
-
-func (mw loggingMiddleware) PatchProfile(ctx context.Context, id string, p Profile) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "PatchProfile", "id", id, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.PatchProfile(ctx, id, p)
-}
-
-func (mw loggingMiddleware) DeleteProfile(ctx context.Context, id string) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "DeleteProfile", "id", id, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.DeleteProfile(ctx, id)
-}
-
-func (mw loggingMiddleware) GetAddresses(ctx context.Context, profileID string) (addresses []Address, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetAddresses", "profileID", profileID, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.GetAddresses(ctx, profileID)
-}
-
-func (mw loggingMiddleware) GetAddress(ctx context.Context, profileID string, addressID string) (a Address, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetAddress", "profileID", profileID, "addressID", addressID, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.GetAddress(ctx, profileID, addressID)
-}
-
-func (mw loggingMiddleware) PostAddress(ctx context.Context, profileID string, a Address) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "PostAddress", "profileID", profileID, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.PostAddress(ctx, profileID, a)
-}
-
-func (mw loggingMiddleware) DeleteAddress(ctx context.Context, profileID string, addressID string) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "DeleteAddress", "profileID", profileID, "addressID", addressID, "took", time.Since(begin), "err", err)
-	}(time.Now())
-	return mw.next.DeleteAddress(ctx, profileID, addressID)
+	return mw.next.GetListByKeyword(ctx, keyword)
 }
